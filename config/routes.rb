f6983d78f4961fda22172842c7d813bc5b306d6c
Mohammed Mahdi Akkouh <akkouh.mahdi@gmail.com>
2013-06-29 00:04:30 +0200

    rails g controller Home

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Membership::Application.routes.draw do
   resources :users
 
+  get "home/index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root to: 'welcome#index'
+  root to: 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
