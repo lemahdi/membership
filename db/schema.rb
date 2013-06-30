831550a8e69777791a4ba4f7fb9cb6cc4adf1bee
Mohammed Mahdi Akkouh <akkouh.mahdi@gmail.com>
2013-06-30 17:28:51 +0200

    rails g migration add_admin_to_users admin:boolean

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130628223926) do
+ActiveRecord::Schema.define(version: 20130630152700) do
 
   create_table "users", force: true do |t|
     t.string   "nom"
     t.string   "prenom"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20130628223926) do
     t.string   "unlock_token"
     t.datetime "locked_at"
     t.string   "authentication_token"
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true
